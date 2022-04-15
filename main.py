4659d030291a162a0e628f77f390e03946a77a82
alex <alexfidalgo10@gmail.com>
2022-04-15 00:15:21 -0300

    criadas as funcoes dinheiro_inicial e estoque_inicial

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 #produtos: refrigerante, agua, chocolate, salgadinhos
-#cedulas: 1, 2, 5, 10
-#moedas: 50, 25, 10, 5
 #botoes: 1, 2, 3, 4, 5
 # 1: compra refrigerante
 # 2: compra agua
 # 3: compra chocolate
 # 4: compra salgadinho
 # 5: devolve troco
-# entrada:
-# - 8 inteiros representando as quantidades iniciais de cada dinheiro
-# - 4 inteiros representando as quantidades dos produtos
-# - 4 inteiros representando os estoques de cada produto
-# - uma sequencia de inteiros com as interaces da maquina com os usuarios.
 
 def dinheiro_inicial():
     # cedulas
+    c_10 = int(input("Cedulas de 10: "))
+    c_5 = int(input("Cedulas de 5: "))
+    c_2 = int(input("Cedulas de 2: "))
+    c_1 = int(input("Cedulas de 1: "))
+    # moedas
+    m_50 = int(input("Moedas de 50: "))
+    m_25 = int(input("Moedas de 25: "))
+    m_10 = int(input("Moedas de 10: "))
+    m_5 = int(input("Moedas de 5: "))
+    return (c_10, c_5, c_2, c_1, m_50, m_25, m_10, m_5)
+
+def estoque_inicial():
+    q_ref = int(input("Estoque inicial de refrigerante: "))
+    q_agu = int(input("Estoque inicial de agua: "))
+    q_cho = int(input("Estoque inicial de chocolate: "))
+    q_sal = int(input("Estoque inicial de salgadinho: "))
+    return (q_ref, q_agu, q_cho, q_sal)
