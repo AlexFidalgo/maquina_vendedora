4634213b3d23a1770799c037e5c16b6ebb656621
alex <alexfidalgo10@gmail.com>
2022-04-15 00:05:18 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#produtos: refrigerante, agua, chocolate, salgadinhos
+#cedulas: 1, 2, 5, 10
+#moedas: 50, 25, 10, 5
+#botoes: 1, 2, 3, 4, 5
+# 1: compra refrigerante
+# 2: compra agua
+# 3: compra chocolate
+# 4: compra salgadinho
+# 5: devolve troco
+# entrada:
+# - 8 inteiros representando as quantidades iniciais de cada dinheiro
+# - 4 inteiros representando as quantidades dos produtos
+# - 4 inteiros representando os estoques de cada produto
+# - uma sequencia de inteiros com as interaces da maquina com os usuarios.
+
+def dinheiro_inicial():
+    # cedulas
